78772619b96c9e53872a6591655cfab14d040b9a
Srirammkm <35733243+Srirammkm@users.noreply.github.com>
2021-03-16 18:03:18 +0530

    Update fhost.py

--- fhost.py
+++ fhost.py
@@ -4,30 +4,32 @@ import boto3
 import json
 
 def tag(ec2):
-    service = []
-    for i in ec2.instances.filter(Filters=[]):
-        for tags in ec2.Instance(i.id).tags:
+    services = []
+    for instance in ec2.instances.filter(Filters=[]):
+        for tags in ec2.Instance(instance.id).tags:
             if tags["Key"] == 'Ansible':
                 name = tags["Value"]
-                if name not in service:
-                    service.append(name)
-    return service
-def hosts(ec2,i):
-    n = {'Name':'tag:Ansible','Values':[i]}
+                if name not in services:
+                    services.append(name)
+    return services
+def hosts(ec2,service):
+#ansible is the tag name in AWS
+    node = {'Name':'tag:Ansible','Values':[service]}
     host = []
-    for i in ec2.instances.filter(Filters=[n]):
-        host.append(i.public_ip_address)
+    for instance in ec2.instances.filter(Filters=[node]):
+        host.append(instance.public_ip_address)
     return host
-def main():
+def aws():
+#change region-name as per your need
     ec2 = boto3.resource("ec2",region_name='ap-southeast-1')
     services = tag(ec2)
     groups = {}
-    for i in services:
-        grp = hosts(ec2,i)
+    for service in services:
+        grp = hosts(ec2,service)
         groups[i] = {
                     'hosts':grp
                     }
     print(groups)
 if __name__ == "__main__":
-    main()
+    aws()
     
